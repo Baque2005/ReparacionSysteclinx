9c96dd6b0a2b7ac7540c1d59b1d6da4c0c734b76
Baque2005 <155196295+Baque2005@users.noreply.github.com>
2025-06-01 18:57:06 -0500

    Descripción de los cambios

--- db.js
+++ db.js
@@ -1,15 +1,11 @@
 const { Pool } = require('pg');
 require('dotenv').config();
 
+const isProduction = process.env.NODE_ENV === 'production';
+
 const pool = new Pool({
-  user: process.env.DB_USER,
-  host: process.env.DB_HOST,
-  database: process.env.DB_DATABASE,
-  password: process.env.DB_PASSWORD,
-  port: process.env.DB_PORT,
-  ssl: {
-    rejectUnauthorized: false,
-  },
+  connectionString: isProduction ? process.env.DATABASE_URL : undefined,
+  ssl: isProduction ? { rejectUnauthorized: false } : false,
 });
 
 module.exports = pool;
